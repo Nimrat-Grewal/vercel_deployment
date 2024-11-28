b05370a50909a68f5e4776f123acdcfe018caaab
Nimrat Grewal <114554275+NimratGrewal@users.noreply.github.com>
2024-11-28 13:03:29 -0500

    error fic

--- pages/csc309-e11.jsx
+++ pages/csc309-e11.jsx
@@ -1,4 +1,12 @@
-<div id="auto-tester">
-  Hi. I am grewa331, and I have learned how to develop a full-stack web app
-  and deploy it!
-</div>
+import React from "react";
+
+export default function E11() {
+    return (
+        <div id="auto-tester">
+      Hi. I am grewa331, and I have learned how to develop a full-stack web app
+      and deploy it!
+    </div>
+    )
+}
+
+
